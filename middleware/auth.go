afac2ad6fc226d68697e2441ddfba3dfd0265f53
left-try <lerest.go@gmail.com>
2024-11-06 03:49:09 +0600

    [Feature] Start logic for authentication through token

--- middleware/auth.go
+++ middleware/auth.go
@@ -12,11 +12,13 @@ func Authenticate(context *gin.Context) {
 		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 		return
 	}
+
 	userId, err := utils.VerifyToken(token)
 	if err != nil {
 		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
 		return
 	}
+
 	context.Set("userId", userId)
 	context.Next()
 }
