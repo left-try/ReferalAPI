af96b97aefa72b1331776c9fbb3bbc021140bf43
left-try <lerest.go@gmail.com>
2024-11-06 03:49:51 +0600

    [Feature] Start logic dor auth

--- routes/user_logic.go
+++ routes/user_logic.go
@@ -14,31 +14,45 @@ func signUp(context *gin.Context) {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request"})
 		return
 	}
+
 	user.Id = 0
 	err = user.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
 		return
 	}
+
 	context.JSON(http.StatusCreated, gin.H{"message": "User created", "user": user})
 }
 
-func logIn(context *gin.Context) {
+func logInByPass(context *gin.Context) {
 	var user models.User
 	err := context.ShouldBindJSON(&user)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request"})
 		return
 	}
+
 	err = user.ValidateUser()
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not log in user"})
 		return
 	}
+
 	token, err := utils.GenerateToken(user.Email, user.Id)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not generate token"})
 		return
 	}
+
 	context.JSON(http.StatusOK, gin.H{"message": "User logged in", "token": token})
+}
+
+func logInByRef(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request"})
+		return
+	}
 }
