224beba04573a6864241b20362769648733b973b
left-try <lerest.go@gmail.com>
2024-11-07 23:56:20 +0600

    [Feature] Update routes

--- routes/routes.go
+++ routes/routes.go
@@ -13,4 +13,7 @@ func Router(server *gin.Engine) {
 	server.POST("/signup", signUp)
 	server.POST("/login_by_pass", logInByPass)
 	server.POST("/login_by_ref", logInByRef)
+
+	server.POST("/create_code", createCode)
+	server.DELETE("/delete_code/:id", deleteCode)
 }
