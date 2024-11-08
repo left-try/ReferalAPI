bdcaee3a3b18abd61dbe52c6ca5ccc5f905904a8
left-try <lerest.go@gmail.com>
2024-11-09 04:15:15 +0600

    [Feature] Start work on swagger

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"referralAPI/middleware"
 )
 
@@ -18,4 +20,6 @@ func Router(server *gin.Engine) {
 	server.GET("/get_referrals/:referrerId", getReferralsByReferrerId)
 	server.POST("/create_code", createCode)
 	server.DELETE("/delete_code/:code", deleteCode)
+
+	server.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
