5476795c242b5b21fc29e66e97c3e54710cf708d
left-try <lerest.go@gmail.com>
2024-11-09 01:12:23 +0600

    [Feature] Update routes to sign up by referral code

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func Router(server *gin.Engine) {
 
 	server.POST("/signup", signUp)
 	server.POST("/login_by_pass", logInByPass)
-	server.POST("/login_by_ref", logInByRef)
+	server.POST("/login_by_ref/:code", signUpByRef)
 
 	server.GET("/get_code_by_email/:email", getCodeByEmail)
 	server.POST("/create_code", createCode)
